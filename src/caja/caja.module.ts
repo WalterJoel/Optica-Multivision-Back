6b2d668befdbf07e45441ef36e91d6c3aaac9f06
WalterJoel <walter.valdivia@ucsp.edu.pe>
2026-04-26 17:58:32 -0500

    ss

--- src/caja/caja.module.ts
+++ src/caja/caja.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { CajaService } from './caja.service';
 import { CajaController } from './caja.controller';
 
+import { Caja } from './entities/caja.entity';
+import { MovimientoCaja } from './entities/movimientoCaja.entity';
+
 @Module({
+  imports: [TypeOrmModule.forFeature([Caja, MovimientoCaja])],
   controllers: [CajaController],
   providers: [CajaService],
 })
